a87a33feb05693f8dd683d84582633ad07c2ccd8
Orcicorn <orcicorn@gmail.com>
2022-09-04 19:57:49 -0400

    Add Expert Gun Repairs

---
+++ Contents/mods/SBTweaksHC/media/lua/client/SBTweaks_Recipes_ExpertRepair.lua
@@ -0,0 +1,14 @@
+-- Super Repair Weapons
+function SBTweaks_SuperRepairGuns(items, result, player)
+    for i=0, items:size() -1 do
+        -- Set Condition to Max Condition
+        if items:get(i):getType() ~= "Screwdriver" then
+            if items:get(i):getType() ~= "Cleaning" then
+                items:get(i):setCondition( items:get(i):getConditionMax() );
+            end
+        end
+    end
+    result:setWeight(0)
+    result:setCondition(0)
+    result:setName("Metal Shavings")
+end
