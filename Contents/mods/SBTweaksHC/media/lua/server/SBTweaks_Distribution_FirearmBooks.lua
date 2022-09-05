9433600a792e1bb2800b9d65262190176ef3d1da
Orcicorn <orcicorn@gmail.com>
2022-09-04 20:37:39 -0400

    Add Firearm/Reloading books

---
+++ Contents/mods/SBTweaksHC/media/lua/server/SBTweaks_Distribution_FirearmBooks.lua
@@ -0,0 +1,99 @@
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookFirearms1");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 1);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookFirearms1");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.7);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookFirearms1");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 1);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookFirearms11");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookFirearms2");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.7);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookFirearms2");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookFirearms22");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 0.7);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookFirearms2");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 0.7);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookFirearms3");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookFirearms3");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.3);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookFirearms3");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookFirearms3");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 0.5);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookFirearms4");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.25);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookFirearmsm4");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.05);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookFirearms4");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 1);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookFirearmsm4");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 1);
+table.insert(ProceduralDistributions["list"]["ArmyStorageGuns"].items, "SBTweaks.BookFirearms4");
+table.insert(ProceduralDistributions["list"]["ArmyStorageGuns"].items, 0.1);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookFirearmsm5");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.25);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookFirearms5");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.05);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookFirearms5");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookFirearms5");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 1);
+table.insert(ProceduralDistributions["list"]["ArmyStorageGuns"].items, "SBTweaks.BookFirearms5");
+table.insert(ProceduralDistributions["list"]["ArmyStorageGuns"].items, 0.1);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookReloading1");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 1);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookReloading1");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.3);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookReloading1");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 1);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookReloading1");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookReloading2");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.7);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookReloading2");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.3);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookReloading2");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 0.7);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookReloading2");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 0.7);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookReloading3");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookReloading3");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.3);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookReloading3");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookReloading3");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 0.5);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookReloadingg4");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.25);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookReloading4");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.05);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookReloading4");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 1);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookReloading4");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 1);
+table.insert(ProceduralDistributions["list"]["ArmyStorageAmmunition"].items, "SBTweaks.BookReloading4");
+table.insert(ProceduralDistributions["list"]["ArmyStorageAmmunition"].items, 0.1);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "SBTweaks.BookReloading5");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.25);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "SBTweaks.BookReloading5");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.05);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "SBTweaks.BookReloading5");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "SBTweaks.BookReloading5");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 1);
+table.insert(ProceduralDistributions["list"]["ArmyStorageAmmunition"].items, "SBTweaks.BookReloading5");
+table.insert(ProceduralDistributions["list"]["ArmyStorageAmmunition"].items, 0.1);
