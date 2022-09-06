03808683484b4239d2682560f369804ed31f23d9
Orcicorn <orcicorn@gmail.com>
2022-09-06 10:27:36 -0400

    Nerf Grimoire spawns more

--- Contents/mods/SBTweaksHC/media/lua/server/SBTweaks_Distribution_Tweak_PawLowLoot.lua
+++ Contents/mods/SBTweaksHC/media/lua/server/SBTweaks_Distribution_Tweak_PawLowLoot.lua
@@ -7,15 +7,15 @@ require 'Vehicles/VehicleDistributions'
 -- SLIGHTLY increase the grimoire spawn rates
 if getActivatedMods():contains("PLLootF") then
     table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, "Base.grimoireclosed");
-    table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, 0.001);
+    table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, 0.0001);
     table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "Base.grimoireclosed");
     table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 0.001);
     table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, "Base.grimoireclosed");
-    table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 0.001);
+    table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 0.0001);
     table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "Base.grimoireclosed");
-    table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.001);
+    table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.0001);
     table.insert(SuburbsDistributions["all"]["bin"].items, "Base.grimoireclosed");
-    table.insert(SuburbsDistributions["all"]["bin"].items, 0.001);
+    table.insert(SuburbsDistributions["all"]["bin"].items, 0.0001);
     table.insert(SuburbsDistributions["all"]["postbox"].items, "Base.grimoireclosed");
-    table.insert(SuburbsDistributions["all"]["postbox"].items, 0.001);
+    table.insert(SuburbsDistributions["all"]["postbox"].items, 0.0001);
 end
